430df4d337c8e6cfe3d42ae0b18e891f5aad0228
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2025-03-24 11:41:42 -0300

    Operadores Lógicos e Relacionais

---
+++ Op_Logicos/Op_Logicos.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+int main(void){
+	printf("%d\n", !0); //1
+	printf("%d\n", !9); //0
+	printf("%d\n", 1 && 0); //0
+	printf("%d\n", 0 || 1); //1
+	printf("%d\n", 2 < 3 || 3 < 2);	//1
+	return 0;
+}
