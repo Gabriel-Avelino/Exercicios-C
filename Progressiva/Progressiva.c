c70b75c6721bcdabd6c5779cb70f3952d603d4e9
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2025-03-26 11:54:44 -0300

    Ultimos exercicios passados pelo professor

---
+++ Progressiva/Progressiva.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+void cp(int n) {
+	static i = 1;
+	if (i <= n){
+		printf("%d ", i);
+		i++;
+		cp(n);
+	}
+}
+
+int main(void) {
+   int n;
+   printf("Num? ");
+   scanf("%d",&n);
+   cp(n);
+   return 0;
+}
---
+++ harmonica/harmonica.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+float h(int n) {
+	if(n == 0) return 0;
+	return 1.0/n + h(n-1.0);
+}
+
+int main(void) {
+   int n;
+   printf("Num? ");
+   scanf("%d",&n);
+   printf("Harmonica = %.2f\n",h(n));
+   return 0;
+}
